439d3149f4f119817dc1290d9c825b48d530dfd3
Rangel Netto <rangelnetto@sympla.com.br>
2021-03-22 09:26:48 -0300

    First commit

---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'Galo', price: 550 },
+    { id: 2, name: 'Crucru', price: 20 },
+    { id: 3, name: 'Sao paulo', price: 220 },
+    { id: 4, name: 'Grêmio', price: 155 }
+]
